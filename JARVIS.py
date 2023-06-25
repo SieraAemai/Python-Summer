8172ccb270c083636e71326dc26096c383bc4c5e
SieraAemai <siera.aemai@gmail.com>
2023-06-25 11:29:00 +0200

    add voices

--- JARVIS.py
+++ JARVIS.py
@@ -1,17 +1,20 @@
 import os
-
-#---------------------
+import speech_recognition as sr
 import pyttsx3
+
+
 engine = pyttsx3.init()
-#engine.say("Текст")
-#engine.runAndWait()
-#---------------------
+
 
 def talk(words):
     print(words)
-    #os.system("say " + words)
     engine.say(words)
     engine.runAndWait()
 
 
 talk("Привет")
+
+def command():
+    r = sr.Recognizer()
+
+command()
--- voices.py
+++ voices.py
@@ -4,10 +4,19 @@ import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty("voices")
 
+# for voice in voices:
+#    print("------")
+#    print(f"Name: {voice.name}")
+#    print(f"ID: {voice.id}")
+#    print(f"Language: {voice.languages}")
+#    print(f"Gender: {voice.gender}")
+#    print(f"Age: {voice.age}")
+
+engine.setProperty("voice", "eng")
 for voice in voices:
-    print("------")
-    print(f"Name: {voice.name}")
-    print(f"ID: {voice.id}")
-    print(f"Language: {voice.languages}")
-    print(f"Gender: {voice.gender}")
-    print(f"Age: {voice.age}")
+    if voice.id == "Microsoft Zira Desktop - English (United States)":
+        engine.setProperty("voice", voice.id)
+rate = engine.getProperty("rate")
+engine.setProperty("rate", rate-40)
+engine.say("Hello")
+engine.runAndWait()
