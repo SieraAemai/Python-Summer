1296c37c341223eecbf90d563c793694f552cbdd
SieraAemai <siera.aemai@gmail.com>
2023-06-30 18:41:18 +0200

    added voice recognizer

--- JARVIS.py
+++ JARVIS.py
@@ -1,4 +1,6 @@
 import os
+import webbrowser
+
 import speech_recognition as sr
 import pyttsx3
 
@@ -12,9 +14,27 @@ def talk(words):
     engine.runAndWait()
 
 
-talk("Привет")
+talk("Hello")
+
 
 def command():
     r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+    task = r.recognize_google(audio, language="en-US").lower() #ru-Ru
+    print("You: "+task)
+    return task
 
-command()
+
+def make_something(ar_task):
+    if ("open" and "site") in ar_task:
+        talk("Ok")
+        url = "https://ituniver.com"
+        webbrowser.open(url)
+
+
+while True:
+    make_something(command())
