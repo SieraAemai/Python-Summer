e4b505b450045f2b838f3254dc6a64e247270492
SieraAemai <siera.aemai@gmail.com>
2023-07-07 18:29:04 +0200

    added .env and finished chatAI

--- ChatAI.py
+++ ChatAI.py
@@ -1,6 +1,12 @@
-import openai
+import os.path
 
-openai.api_key = "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"
+import openai
+from dotenv import load_dotenv as ld
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    ld(dotenv_path)
+openai.api_key = os.getenv("api_key")
 
 models = openai.Model.list()
 #print(models)
@@ -10,4 +16,10 @@ def handle_input(user_input):
         model="gpt-3.5-turbo"
         , messages=[{"role": "user", "content": user_input}]
     )
-    return  copletion
+    return copletion
+
+
+#handle_input(input()).choices[0].message.content
+
+while True:
+    print(handle_input(input("You: ")).choices[0].message.content)
